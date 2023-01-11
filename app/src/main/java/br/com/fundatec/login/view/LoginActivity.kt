5280549b397364cfc3786e4ee4ee8d08993552e7
Dionata Ferraz <dionata.ferraz@ifood.com.br>
2023-01-11 20:10:26 -0300

    feat: Adding data binding

--- app/src/main/java/br/com/fundatec/login/view/LoginActivity.kt
+++ app/src/main/java/br/com/fundatec/login/view/LoginActivity.kt
@@ -8,25 +8,29 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.constraintlayout.widget.ConstraintLayout
 import br.com.fundatec.R
+import br.com.fundatec.databinding.ActivityLoginBinding
 import br.com.fundatec.login.HomeActivity
 import br.com.fundatec.login.presentation.LoginViewModel
 import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<TextView>(R.id.tv_text).setOnClickListener {
+        binding.tvText.setOnClickListener {
             showToast()
             showSnack()
         }
     }
 
-    private fun showToast(){
+    private fun showToast() {
         Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
     }
 
---
+++ home/build/generated/source/buildConfig/debug/br/com/fundatec/home/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.fundatec.home;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "br.com.fundatec.home";
+  public static final String BUILD_TYPE = "debug";
+}
